2eaa7a499d1775607f0b6e803d44261433d622a6
Nikita1814 <kravchenko1814@gmail.com>
2021-08-16 01:16:51 +0300

    feat: add function for the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,54 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    let digits = [
+        "",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    let ty = [
+        "",
+        "",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+    let strnum = number.toString();
+    if (number === 0) {
+        return "zero";
+    }
+    if (number < 20) {
+        return digits[number];
+    }
+    if (number >= 20 && number <= 99) {
+        return `${ty[+strnum[0]]} ${digits[+strnum[1]]}`.trim();
+    }
+    if (number > 99 && number % 100 < 20) {
+        return `${digits[+strnum[0]]} hundred ${digits[number % 100]}`.trim();
+    }
+    if (number > 99 && number % 100 >= 20) {
+        return `${digits[+strnum[0]]} hundred ${ty[+strnum[1]]} ${
+            digits[+strnum[2]]
+        }`.trim();
+    }
+};
